8cb59d718e3cc46d984052b5f6bdd8915fc55ae9
a954217436 <954217436@qq.com>
2023-02-02 15:52:57 +0800

    美化了画框颜色

--- src/lib/tracker/tracker_utils/tracker_utils.h
+++ src/lib/tracker/tracker_utils/tracker_utils.h
@@ -83,7 +83,7 @@ static void draw_trk_objects(cv::Mat& image, const TrkObjectList& objects)
             color = cv::Scalar(0,238,238);      // yellow
             break;
         case ACCS_SIGN1:
-            color = cv::Scalar(144,238,144);    // green
+            color = cv::Scalar(147,20,255);     // pink
             break;
         default:
             color = cv::Scalar(205,205,180);    // gray
@@ -91,13 +91,14 @@ static void draw_trk_objects(cv::Mat& image, const TrkObjectList& objects)
         }
 
         cv::Scalar txt_color = cv::Scalar(255,255,255);
-        cv::rectangle(image, obj.rect, color, 2);
+        cv::rectangle(image, obj.rect, color, 3);
 
 #ifdef DRAW_TRACK_TEXT_INFOS
         char text[256];
         sprintf(text, "%s_%d", obj.GetClassname().c_str(), obj.track_id);
         int baseLine = 0;
-        cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.4, 1, &baseLine);
+        float txt_size = 0.5;
+        cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, txt_size, 1, &baseLine);
         cv::Scalar txt_bk_color = color * 0.7;
 
         int x = obj.rect.x;
@@ -111,7 +112,7 @@ static void draw_trk_objects(cv::Mat& image, const TrkObjectList& objects)
         cv::rectangle(image, cv::Rect(cv::Point(x, y-label_size.height), cv::Size(label_size.width, label_size.height + baseLine)),
                       txt_bk_color, -1);
 
-        cv::putText(image, text, cv::Point(x, y), cv::FONT_HERSHEY_SIMPLEX, 0.4, txt_color, 1);
+        cv::putText(image, text, cv::Point(x, y), cv::FONT_HERSHEY_SIMPLEX, txt_size, txt_color, 1);
 #endif  //  DRAW_TRACK_TEXT_INFOS
     }
 }
